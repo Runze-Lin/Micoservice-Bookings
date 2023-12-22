82f7800bff893eddabc768d4a4c36f9b74b81f9f
Isa Wang <74472229+IsaWang-05@users.noreply.github.com>
2023-12-21 19:49:18 -0500

    Update middleservice.py

--- middleservice.py
+++ middleservice.py
@@ -3,7 +3,6 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        start_time = time.time()
 
         # log request details
         request_details = f"Request: {request.method} {request.url}"
@@ -12,11 +11,9 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         # call the next middleware or route handler
         response = await call_next(request)
 
-        # calculation
-        process_time = time.time() - start_time
 
         # log response details
-        response_details = f"Response: Status {response.status_code}, Duration {process_time:.2f} seconds"
+        response_details = f"Response: Status {response.status_code}"
         print(response_details)
 
         return response
